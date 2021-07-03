6d7f72e7d9201a446a244145fc611115c9c2b142
Prasoon Soni <prasoonsoni03@gmail.com>
2021-07-03 19:39:30 +0530

    Updated Get Started Screen.

--- app/src/main/java/com/tfd/ffcsez/GetStartedActivity.java
+++ app/src/main/java/com/tfd/ffcsez/GetStartedActivity.java
@@ -10,24 +10,29 @@ import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.cuberto.liquid_swipe.LiquidPager;
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator;
 import com.tfd.ffcsez.adapters.ViewPagerAdapter;
 
-public class GetStartedActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class GetStartedActivity extends AppCompatActivity {
+    @BindView(R.id.wormDotIndicator) WormDotsIndicator wormDotsIndicator;
+    @BindView(R.id.pager) ViewPager2 pager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+        ButterKnife.bind(this);
 
-        ViewPager2 pager = findViewById(R.id.pager);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getColor(R.color.dark_purple));
+
         FragmentManager fragmentManager = getSupportFragmentManager();
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(fragmentManager, getLifecycle());
         pager.setAdapter(viewPagerAdapter);
+        wormDotsIndicator.setViewPager2(pager);
     }
 
-    public void updateStatusBarColor(String color) {// Color must be in hexadecimal format
-        Window window = getWindow();
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(Color.parseColor(color));
-    }
 }
